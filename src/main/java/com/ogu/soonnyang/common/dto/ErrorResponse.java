0c98a73a2fd23c98e3757300de774a1e2ef2493b
Ogu1208 <kdasunb@naver.com>
2024-03-11 21:24:09 +0900

    [feat] ErrorResponse DTO 구현

---
+++ src/main/java/com/ogu/soonnyang/common/dto/ErrorResponse.java
@@ -0,0 +1,28 @@
+package com.ogu.soonnyang.common.dto;
+
+import com.ogu.soonnyang.common.exception.ErrorCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ErrorResponse {
+
+    private String message;
+    private String code;
+    private int status;
+    private String detail;
+
+    public ErrorResponse(ErrorCode code) {
+        this.message = code.getMessage();
+        this.status = code.getStatus();
+        this.code = code.getCode();
+        this.detail = code.getDetail();
+    }
+
+    public static ErrorResponse of(ErrorCode code) {
+        return new ErrorResponse(code);
+    }
+}
