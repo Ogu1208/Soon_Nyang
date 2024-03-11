dd643febf748a68eb2737f6addc23a0f3f1967e8
Ogu1208 <kdasunb@naver.com>
2024-03-11 19:12:54 +0900

    first commit

---
+++ src/main/java/com/ogu/soonnyang/SoonNyangApplication.java
@@ -0,0 +1,13 @@
+package com.ogu.soonnyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoonNyangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoonNyangApplication.class, args);
+    }
+
+}
