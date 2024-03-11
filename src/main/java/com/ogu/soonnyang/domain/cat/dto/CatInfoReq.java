f4fcded0d8c4d3477e5910750ec36f66e668c01a
Ogu1208 <kdasunb@naver.com>
2024-03-11 21:23:25 +0900

    [feat] CatInfoReq DTO 구현

---
+++ src/main/java/com/ogu/soonnyang/domain/cat/dto/CatInfoReq.java
@@ -0,0 +1,21 @@
+package com.ogu.soonnyang.domain.cat.dto;
+
+import com.ogu.soonnyang.domain.cat.entity.type.CatGender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class CatInfoReq {
+    private String name;
+    private Integer age;
+    private CatGender gender;
+    private MultipartFile image;
+    private Float lat;
+    private Float lng;
+}
