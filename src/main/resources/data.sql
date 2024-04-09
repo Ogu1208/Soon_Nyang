dd9b2cc33907b0b0aee8f61fe285c06a23536b47
Ogu1208 <kdasunb@naver.com>
2024-04-09 14:48:05 +0900

    fix: data.sql 초기화 문법 오류 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,26 +1,26 @@
 -- Cat 데이터 샘플 추가
-INSERT INTO Cat (name, age, gender, follower_cnt, image_url, is_active)
+INSERT INTO cat (name, age, gender, follower_cnt, image_url, is_active)
 VALUES ('루루', '3살', 'FEMALE', 100, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgPW2VrodfqgvWKVlM3PGjhYPTMVbEeFurj-6YsW8DlA&s', 'ACTIVE');
 
-INSERT INTO Cat (name, age, gender, follower_cnt, image_url, is_active)
+INSERT INTO cat (name, age, gender, follower_cnt, image_url, is_active)
 VALUES ('톰', '2살', 'MALE', 150, 'https://flexible.img.hani.co.kr/flexible/normal/743/478/imgdb/original/2023/0208/8916758411258711.jpg', 'ACTIVE');
 
-INSERT INTO Cat (name, age, gender, follower_cnt, image_url, is_active)
+INSERT INTO cat (name, age, gender, follower_cnt, image_url, is_active)
 VALUES ('제리', '7개월', 'MALE', 80, 'https://cdn.ntoday.co.kr/news/photo/202007/73742_46548_1519.jpg', 'ACTIVE');
 
-INSERT INTO Cat (name, age, gender, follower_cnt, image_url, is_active)
+INSERT INTO cat (name, age, gender, follower_cnt, image_url, is_active)
 VALUES ('미미', '1살', 'FEMALE', 200, 'https://ojsfile.ohmynews.com/STD_IMG_FILE/2022/0311/IE002955775_STD.jpg', 'INACTIVE');
 
-INSERT INTO Cat (name, age, gender, follower_cnt, image_url, is_active)
+INSERT INTO cat (name, age, gender, follower_cnt, image_url, is_active)
 VALUES ('순냥이', '5살', 'MALE', 120, 'https://img.seoul.co.kr//img/upload/2023/04/20/SSC_20230420134023.jpg', 'ACTIVE');
 
-INSERT INTO MEMBER(email, password, nickname, created_at)
+INSERT INTO member(email, password, nickname, created_at)
 VALUES ('kdasunb6@gmail.com', 'snsn22@@', '순냥이 집사 호소인', '2024-01-01 12:00:00');
 
-INSERT INTO MEMBER(email, password, nickname, created_at)
+INSERT INTO member(email, password, nickname, created_at)
 VALUES ('snsnjerry@gmail.com', 'snsn22@!', '루루는 해적선장', '2023-02-11 12:00:00');
 
-INSERT INTO POST(latitude, longitude, content, image, like_count, member_id, cat_id, created_at)
+INSERT INTO post(latitude, longitude, content, image, like_count, member_id, cat_id, created_at)
 VALUES
     (36.772250443829144, 126.93182972875724 , '미랩 앞에서 만난 귀여운 루루!', 'https://www.fitpetmall.com/wp-content/uploads/2023/09/shutterstock_2205178589-1-1.png', 35 , 1, 1, '2024-04-08 12:00:00'),
     (36.772204877679, 126.93094487795577, '오늘도 톰 만남 ㅠㅠ 행복하다 무서운척하는데 귀여움!', 'https://image.edaily.co.kr/images/photo/files/NP/S/2024/01/PS24010200845.jpg', 60, 1, 2, '2024-04-08 13:00:00'),
