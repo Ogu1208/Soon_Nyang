fbad55c2c2648b7d55e71a4a9162a81239b4b67f
Ogu1208 <kdasunb@naver.com>
2024-04-29 06:42:11 +0900

    fix: test Bean 오류 수정

--- src/test/java/com/ogu/soonnyang/SoonNyangApplicationTests.java
+++ src/test/java/com/ogu/soonnyang/SoonNyangApplicationTests.java
@@ -3,7 +3,7 @@ package com.ogu.soonnyang;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SoonNyangApplicationTests.class)
 class SoonNyangApplicationTests {
 
     @Test
